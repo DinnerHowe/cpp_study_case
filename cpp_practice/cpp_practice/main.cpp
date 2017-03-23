49ea271d2dc3903abc50da72a208b47ba02076d6
DinnerHowe <xu001314@163.com>
2017-03-23 23:17:47 +0800

    1

---
+++ cpp_practice/cpp_practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_practice
+//
+//  Created by 徐志浩 on 2017/3/23.
+//  Copyright © 2017年 徐志浩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
