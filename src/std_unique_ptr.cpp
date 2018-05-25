d4352cd3061f052dad47529a9e7488e6bbdf6713
DinnerHowe <xu001314@163.com>
2018-05-25 17:24:54 +0800

    reform

---
+++ src/std_unique_ptr.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+//   unique_ptr<int> spinner;
+//   spinner = unique_ptr<int>(new int(5));
+//   cout << spinner <<endl;
+}
--- std_unique_ptr.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, char **argv)
-{
-   unique_ptr<int> spinner;
-   spinner = unique_ptr<int>(new int(5));
-   cout << spinner <<endl;
-}
